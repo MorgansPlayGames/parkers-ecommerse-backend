3bb5d4e8b8bd4bfbc1343bb09764da95e233751e
Parker Morgan <jpmaster13@msn.com>
2020-12-11 22:40:41 -0700

    Added post category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,6 +37,14 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryPost = await Category.create({
+      category_name : req.body.category_name,
+    });
+    res.status(200).json(categoryPost);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
