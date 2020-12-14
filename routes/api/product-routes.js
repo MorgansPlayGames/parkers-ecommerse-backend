3a4922075d5d558e37a90831676361a7030d786c
Parker Morgan <jpmaster13@msn.com>
2020-12-13 20:33:32 -0700

    delete product added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,6 +21,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  
 });
 
 // create new product
@@ -97,8 +98,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    if (!deleteProduct){
+      res.status(400).json({ message: "No product by this Id"})
+      return;
+    }
+
+    res.status(200).json(deleteProduct);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
